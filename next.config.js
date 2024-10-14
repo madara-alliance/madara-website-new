5026c5cf01fab4573b8d06ecb0e4fa9fae1b88c7
Samrendra <samrendrakumarsingh01@gmail.com>
2024-10-14 14:38:48 +0530

    adding redirect to launcher

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/launcher.sh',
+        destination: 'https://raw.githubusercontent.com/madara-alliance/madara/refs/heads/main/scripts/launcher',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
